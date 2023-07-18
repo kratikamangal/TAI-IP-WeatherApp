6356f086572829287ac1ffef9eb969a5fb292ff2
Kratika <kratikamangal11@gmail.com>
2023-07-18 11:21:59 +0530

    weather app using reactjs

--- src/App.js
+++ src/App.js
@@ -7,22 +7,11 @@ function App() {
     <React.Fragment>
       <div className="container">
         <CurrentLocation />
-      </div>
-      <div className="footer-info">
-        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
-          {/* Download Source Code */}
-        </a>{" "}
-        {/* | Developed by{" "} */}
-        <a target="_blank" href="https://www.gauravghai.dev/">
-          {/* Gaurav Ghai */}
-        </a>{" "}
-        {/* | Powered by{" "} */}
-        <a target="_blank" href="https://www.htmlhints.com/">
-          {/* HTML HINTS */}
-        </a>
-      </div>
+      </div>  
     </React.Fragment>
   );
 }
 
 export default App;
+
+ 
